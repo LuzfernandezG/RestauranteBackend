47f75617295889a1d1bed7e8069ea5e2ad981d96
LuzfernandezG <fernandezluz1995@gmail.com>
2024-05-08 18:36:25 -0500

    cambio mayo reciente

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,13 +1,13 @@
 
+//importaciones
 import bcryptjs from "bcryptjs"; //libreria que permitira la encriptacion de contraseñas
 import jsonwebtoken from "jsonwebtoken";//maneja de contraseña para login tipo token 
 import dotenv from "dotenv";//libreria para variables de entorno 
 import connection from "./db.js";
-
-// Aquí puedes utilizar la conexión a la base de datos
+import { response } from 'express';
 dotenv.config();
 
-
+//Funcion para login
 async function login(req, res) {
     const user = req.body.user;
     const password = req.body.password;
@@ -29,7 +29,6 @@ async function login(req, res) {
             }
 
             //comparacion de la contraseña con bcrypts
-
             const usuarioRevisar = results[0];
             bcryptjs.compare(password, usuarioRevisar.password, (compareError, logincorrecto) => {
                 if (compareError) {
@@ -53,7 +52,13 @@ async function login(req, res) {
                 };
 
                 res.cookie("jwt", token, cookieOption);
-                res.send({ status: "ok", message: "Usuario loggeado", redirect: "/admin" });
+                if (usuarioRevisar.modo === 1) {
+                    // Si el modo es 1, redirigir al admin
+                    res.send({ status: "ok", message: "Usuario loggeado", token, redirect: "/admin" });
+                } else {
+                    // Si el modo es 0, redirigir al home
+                    res.send({ status: "ok", message: "Usuario loggeado", token, redirect: "/home" });
+                }
             });
         });
     } catch (error) {
@@ -61,9 +66,7 @@ async function login(req, res) {
         return res.status(500).send({ status: "Error", message: "Error interno del servidor" });
     }
 }
-
-
-
+//Funcion para registrar usuario
 async function register(req, res) {
     try {
         const { user,email,password } = req.body;
@@ -99,7 +102,7 @@ async function register(req, res) {
                 }
 
                 // Envía la respuesta de éxito
-                res.status(201).send({ status: "ok", message: `Usuario ${user} agregado`, redirect: "/" });
+                res.status(201).send({ status: "ok", message: `Usuario ${user} agregado`, redirect: "/login" });
             });
         });
     } catch (error) {
@@ -107,8 +110,159 @@ async function register(req, res) {
         res.status(500).send({ status: "Error", message: "Error interno del servidor" });
     }
 }
-//exporta los metodos para poder ser ejecutados en otros archivos
+
+//Funcion para cerrar sesion
+async function logout(req, res = response) {
+    try {
+        // Eliminar la cookie jwt del cliente
+        res.clearCookie("jwt");
+
+        // Envía una respuesta exitosa
+        res.status(200).send({ status: "ok", message: "Usuario deslogueado exitosamente" });
+    } catch (error) {
+        console.error("Error al cerrar sesión:", error);
+        res.status(500).send({ status: "Error", message: "Error interno del servidor" });
+    }
+}
+
+/*MANEJO DE MESAS*/
+
+//Funcion para crear mesa
+function crearMesa(nombre, numero, descripcion) {
+    const sql = `INSERT INTO mesas (nombre, numero, descripcion) VALUES (?, ?, ?)`;
+    connection.query(sql, [nombre, numero, descripcion], (err, result) => {
+      if (err) throw err;
+      console.log(`Se ha creado una nueva mesa con ID: ${result.insertId}`);
+    });
+  }
+  
+
+  // Función para modificar una mesa por número de mesa
+  function modificarMesaPorNumero(numeroMesa, nuevosDatos) {
+    const sql = `UPDATE mesas SET ? WHERE numero = ?`;
+    connection.query(sql, [nuevosDatos, numeroMesa], (err, result) => {
+      if (err) throw err;
+      console.log(`Se han modificado ${result.changedRows} mesa(s)`);
+    });
+  }
+  
+  // Función para imprimir todas las mesas
+  function imprimirMesas(callback) {
+    const sql = `SELECT * FROM mesas`;
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.error("Error al imprimir las mesas:", err);
+        callback([]);
+      } else {
+        console.log("Mesas encontradas:", result);
+        callback(result);
+      }
+    });
+  }
+  
+  // Función para eliminar una mesa por número de mesa
+  function eliminarMesaPorNumero(numeroMesa) {
+    const sql = `DELETE FROM mesas WHERE numero = ?`;
+    connection.query(sql, [numeroMesa], (err, result) => {
+      if (err) throw err;
+      console.log(`Se han eliminado ${result.affectedRows} mesa(s)`);
+    });
+  }
+
+//Funcion para obtener mesas no reservadas apartir de una fecha
+  const obtenerMesasDisponibles = async (req, res) => {
+    const { fecha } = req.query;
+    
+    try {
+      // Consulta SQL para obtener las mesas que no están reservadas para la fecha especificada
+      const sql = `
+        SELECT *
+        FROM mesas
+        WHERE numero NOT IN (
+          SELECT numero_mesa
+          FROM reservas
+          WHERE fecha = ?
+        )
+      `;
+      connection.query(sql, [fecha], (error, results, fields) => {
+        if (error) {
+          console.error("Error al obtener las mesas disponibles:", error);
+          return res.status(500).send({ status: "Error", message: "Error interno del servidor" });
+        }
+        res.status(200).json(results);
+      });
+    } catch (error) {
+      console.error("Error al obtener las mesas disponibles:", error);
+      return res.status(500).send({ status: "Error", message: "Error interno del servidor" });
+    }
+  }
+
+/*FUNCIONES PARA RESERVACION DE MESAS*/
+
+//Funcion para reservar mesa
+function reservarMesa(nombreReservante, fecha, numeroMesa, motivo) {
+    verificarMesaDisponible(numeroMesa, fecha, (disponible) => {
+        if (!disponible) {
+            console.error("La mesa no está disponible para esa fecha");
+            return;
+        }
+        const insertSql = `INSERT INTO reservas (nombre_reservante, fecha, numero_mesa, motivo) VALUES (?, ?, ?, ?)`;
+        connection.query(insertSql, [nombreReservante, fecha, numeroMesa, motivo], (insertErr, insertResult) => {
+            if (insertErr) {
+                console.error("Error al reservar la mesa:", insertErr);
+                throw insertErr;
+            }
+            console.log(`Se ha realizado la reserva con ID: ${insertResult.insertId}`);
+        });
+    });
+}
+//Funcion para actualizar estado de mesa
+function actualizarEstadoMesa(numeroMesa, estado) {
+    const sql = `UPDATE mesas SET estado = ? WHERE numero = ?`;
+    connection.query(sql, [estado, numeroMesa], (err, result) => {
+      if (err) throw err;
+      console.log(`Se ha actualizado el estado de la mesa ${numeroMesa} a ${estado}`);
+    });
+}
+
+//Funcion comparativa de numero y fecha para saber si esta disponible
+function verificarMesaDisponible(numeroMesa, fecha, callback) {
+    const sql = `SELECT * FROM reservas WHERE numero_mesa = ? AND fecha = ?`;
+    connection.query(sql, [numeroMesa, fecha], (err, result) => {
+      if (err) {
+        console.error("Error al verificar la disponibilidad de la mesa:", err);
+        callback(false);
+      } else {
+        callback(result.length === 0); 
+      }
+    });
+}
+//Funcion para imprimir las reservas 
+    function imprimirReservas(callback) {
+        const sql = `SELECT * FROM reservas`;
+        connection.query(sql, (err, result) => {
+          if (err) {
+            console.error("Error al imprimir las reservas:", err);
+            callback([]);
+          } else {
+            console.log("Reservas encontradas:", result);
+            callback(result);
+          }
+        });
+      }
+
+/*EXPORTACION DE METODOS*/ 
 export const methods = {
     login,
-    register
+    register,
+    logout,
+    crearMesa,
+    modificarMesaPorNumero,
+    imprimirMesas,
+    eliminarMesaPorNumero,
+    reservarMesa,
+    actualizarEstadoMesa,
+    verificarMesaDisponible,
+    obtenerMesasDisponibles,
+    imprimirReservas
 }
--- app/index.js
+++ app/index.js
@@ -1,34 +1,69 @@
-//dirname
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+/*IMPORTACIONES */
 import express from "express";
-import cookieParser from "cookie-parser";
-import {methods as autenthication} from"./controllers/authentication.controller.js";
-
+import cookieParser from "cookie-parser"; 
+import { methods as autenthication } from "./controllers/authentication.controller.js"; //Toma las funciones que se exportaron en authetication controllers 
+import cors from 'cors'; //sirve para  que haya una transmicion de datos entre backend-frontend =COMUNICACION DE PUERTOS
 
 //servidor
-const app=express();
-app.set("port",4000);
+const app = express();
+app.set("port", 4000);
 app.listen(app.get("port"));
-console.log("servidor corriendo en puerto",app.get("port")); //abre el puerto
+console.log("servidor corriendo en puerto", app.get("port")); //abre el puerto
 
 //configuracion
-app.use(express.static(__dirname + "/public")); //aginar archivos estaticos
-app.use(express.json());//permite mirar los datos mandados en el registro
+app.use(express.json()); //permite mirar los datos mandados en el registro
 app.use(cookieParser());
+app.use(cors());//comunicacion fronted-backend
 
 //rutas
-app.get("/", (req,res)=> res.sendFile(__dirname +"/pages/login.html"));
-app.get("/register", (req,res)=> res.sendFile(__dirname +"/pages/register.html"));
-app.get("/admin",(req,res)=> res.sendFile(__dirname +"/pages/admin/admin.html"));
-app.post("/api/register",autenthication.register);
-app.post("/api/login",autenthication.login);
-// Ruta para cerrar sesión
-app.get("/logout", (res) => {
-    // Eliminar la cookie
-    res.clearCookie("jwt");
-    // Redirigir al inicio de sesión
-    res.redirect("/");
+//login
+app.post("/api/register", autenthication.register);
+app.post("/api/login", autenthication.login);
+app.get("/api/logout", autenthication.logout);
+
+//manejo de mesas
+app.post("/api/mesas", (req, res) => {
+    const { nombre, numero, descripcion } = req.body;
+    autenthication.crearMesa(nombre, numero, descripcion);
+    res.send("Mesa creada exitosamente");
+  });
+  app.put("/api/mesas/:numero", (req, res) => {
+    const { numero } = req.params;
+    const { nombre, descripcion } = req.body;
+    autenthication.modificarMesaPorNumero(numero, { nombre, descripcion });
+    res.send(`Mesa con número ${numero} modificada exitosamente`);
+  });
+  app.get("/api/mesas", (req, res) => {
+    autenthication.imprimirMesas((mesas) => {
+      res.json(mesas);
+    });
+  });
+  app.delete("/api/mesas/:numero", (req, res) => {
+    const { numero } = req.params;
+    autenthication.eliminarMesaPorNumero(numero);
+    res.send(`Mesa con número ${numero} eliminada exitosamente`);
+  });
+  
+//reserva de mesas
+app.post("/api/reservar-mesa", (req, res) => {
+  const { nombreReservante, fecha, numeroMesa ,motivo } = req.body;
+  
+  autenthication.verificarMesaDisponible(numeroMesa, fecha, (disponible) => {
+      if (!disponible) {
+          // La mesa no está disponible para la fecha especificada
+          return res.status(400).send("La mesa no está disponible para la fecha especificada");
+      }
+
+      // La mesa está disponible, proceder con la reserva
+      autenthication.reservarMesa(nombreReservante, fecha, numeroMesa, motivo);
+      res.send("Mesa reservada exitosamente");
+  });
 });
 
+app.get("/api/mesas-disponibles", autenthication.obtenerMesasDisponibles);
+
+app.get("/api/imprimir-reservas", (req, res) => {
+  autenthication.imprimirReservas((reservas) => {
+    res.json(reservas);
+  });
+});
